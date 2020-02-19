88ac478036c1d3a2d7e7403edeb25b3b047eb6ac
c-astle35 <61034457+c-astle35@users.noreply.github.com>
2020-02-18 22:49:33 -0800

    Create basic_lin_alg.py

---
+++ basic_lin_alg.py
@@ -0,0 +1,8 @@
+from numpy import linalg
+
+'''
+TODO: 
+work on this: 
+  - https://docs.scipy.org/doc/numpy/reference/routines.linalg.html
+ & this:
+  -https://spark.apache.org/docs/latest/api/python/pyspark.html
